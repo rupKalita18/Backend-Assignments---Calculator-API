f502f2a6c22b47de05f30d4543835b9daaadef66
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-11-17 12:16:59 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ app.post("/sub", (req, res) => {
     let diff = num1 - num2;
     res.send({
       status: "success",
-      message: "The difference of given two numbers",
+      message: "the difference of given two numbers",
       diffrence: diff
     });
     return;
